8975bba0514bda8161cffce1f92e9ddd405e8a7c
Yagnik Patel <coderisaddicted@gmail.com>
2021-07-28 14:32:03 +0530

    enhacements

--- js/mobImgSlide.js
+++ js/mobImgSlide.js
@@ -5,10 +5,12 @@ if (mediaQuery.matches) {
   let windowWidth = window.innerWidth;
 
   window.addEventListener("scroll", function () {
+    let left = -300;
     let valueY = window.scrollY;
-    let needValue = (((valueY / windowHeight) * windowWidth) / 8 / 100) * 8;
+    let needValue = (((valueY / windowHeight) * windowWidth) / 40) * 16;
+    console.log(needValue);
     if (valueY > 400) {
-      document.querySelector("#galaxyOps").style.left = `${needValue}%`;
+      document.querySelector("#galaxyOps").style.left = `${left + needValue}px`;
     }
   });
 }
