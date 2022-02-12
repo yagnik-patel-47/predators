e66100297c237f7f5ae2491685ff6adf7b63e823
Yagnik Patel <coderisaddicted@gmail.com>
2022-02-12 17:04:43 +0530

    some changes

--- js/mobImgSlide.js
+++ js/mobImgSlide.js
@@ -5,10 +5,9 @@ if (mediaQuery.matches) {
   let windowWidth = window.innerWidth;
 
   window.addEventListener("scroll", function () {
-    let left = -300;
+    let left = -330;
     let valueY = window.scrollY;
     let needValue = (((valueY / windowHeight) * windowWidth) / 40) * 16;
-    console.log(needValue);
     if (valueY > 400) {
       document.querySelector("#galaxyOps").style.left = `${left + needValue}px`;
     }
